b26b4f4d76372ca902f88f5540305c67a202c82a
Viktor Zotov <ZotovV777@yandex.ru>
2022-12-12 19:55:20 +0300

    fix 8 step

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -4,14 +4,15 @@ from gendiff.format.stylish import stylish
 from gendiff.format.plain import plain
 from gendiff.format.format_json import format_json
 
+
 def parse():
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.'
     )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-f', '--format', choices=['plain', 'json'], default=stylish,
-                        help='set format of output')
+    parser.add_argument('-f', '--format', choices=['plain', 'json'],
+                        default=stylish, help='set format of output')
     args = parser.parse_args()
     if args.format == 'plain':
         print((generate_diff(args.first_file, args.second_file, plain)))
